de97dbb178e0822500a22f49b29da4c33f93f45f
Taoalex542 <darbellay.tao@gmail.com>
2024-11-06 10:49:40 +0100

    activation du controle auto-intersection

--- controles/auto_intersection/auto_intersection.py
+++ controles/auto_intersection/auto_intersection.py
@@ -73,10 +73,6 @@ def nb_for_tuple(self, str):
 # execution du controle
 def auto_intersection(self, func):
     nom_controle = "auto intersection"
-    # objets_controle = ["limite_administrative", "ligne_frontalière", "tronçon_hydrographique", "limite_terre_mer"
-    #            , "histolitt", "ligne_électrique", "canalisation", "construction_linéaire", "ligne_orographique"
-    #            , "troncon_de_route", "densification_des_chemins", "tronçon_de_voie_ferrée", "transport_par_câble", "voie_de_triage"
-    #            , "itinéraire_ski_de_randonnée", "haie", "ligne_caractéristique", "limites_diverses", "modification_d_attribut"]
     for item in self.dlg_controles.treeWidget.findItems("auto intersection", QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive):
         # vérifie si le contrôle "auto_intersection" est coché et si il existe des objets de type Ligne
         if item.checkState(0) == 2:
@@ -103,27 +99,16 @@ def auto_intersection(self, func):
                         # si la couche est présente dans la liste et qu'elle est cochée 
                         if layers.name() == items[0] and items[2] == QtCore.Qt.Checked: #(QtCore.Qt.Checked == 2)
                             # récupère les informations des couches
-                            # print(layers.fields().names())
                             for f in layers.getFeatures():
                                 # récupère la géométrie dans ces infos
                                 geom = f.geometry()
-                                # print('Area :', geom.area())
-                                # print('Perimeter :', geom.length())
-                                # print('Type :', QgsWkbTypes.displayString(geom.wkbType()))
                                 # récupère les informations nécéssaires dans la géométrie tel que le nom, le type, et les points
                                 for part in geom.parts():
                                     # mets a jour le progrès de la bar de progrès
                                     if ("LineString" not in QgsWkbTypes.displayString(part.wkbType()) and "Polygon" not in QgsWkbTypes.displayString(part.wkbType())):
                                         break
                                     bar.setValue(int(items_done / quantity * 100))
-                                    # mets comme type de données l'ESPG 2154
-                                    # part.transform(QgsCoordinateTransform(
-                                    #     QgsCoordinateReferenceSystem("IGNF:LAMB93"),
-                                    #     QgsCoordinateReferenceSystem("EPSG:2154"),
-                                    #     QgsProject.instance()))
                                     # parse le WKT de la géométrie pour avoir accès a chaque chiffre en tant que floats
-                                    print(part.asWkt())
-                                    return
                                     nums = re.findall(r'\-?[0-9]+(?:\.[0-9]*)?', part.asWkt()) # regex cherche entre chaque virgule: au moins un chiffre, puis un point, puis une chiffre si il y en a un, avec des parenthèses optionellement
                                     coords = tuple(zip(*[map(float, nums)] * nb_for_tuple(self, part.asWkt()))) # récupère les coordonnées en float et les ajoutes dans un tableau de floats pour une utilisation facile des données antérieurement
                                     # lance le controle rebroussement
