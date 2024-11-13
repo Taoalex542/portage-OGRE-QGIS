b8c38d05afdafde6b9041932bff760390f4692f7
Taoalex542 <darbellay.tao@gmail.com>
2024-11-13 15:27:03 +0100

    fix d'un crash

--- controles_ign_dialog.py
+++ controles_ign_dialog.py
@@ -119,16 +119,3 @@ class choix_precis(QtWidgets.QDialog, FORM_CLASS1):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
-        
-FORM_CLASS1, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__),'ui/patience.ui'))
-class patience(QtWidgets.QDialog, FORM_CLASS1):
-
-    def __init__(self, parent=None):
-        """Constructor."""
-        super(patience, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
-        self.setupUi(self)
