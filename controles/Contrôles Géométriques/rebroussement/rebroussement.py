30c1da20125d1734d6f39cad5751f46493df2640
Taoalex542 <darbellay.tao@gmail.com>
2024-11-15 11:53:55 +0100

    optimisation des paramètres du contrôle Rebroussement

--- controles/Contrôles Géométriques/rebroussement/rebroussement.py
+++ controles/Contrôles Géométriques/rebroussement/rebroussement.py
@@ -23,30 +23,28 @@ def read(self):
         if line_number >= 5:
             # lis la première ligne pour le paramètre de l'angle contenu dans paramètre[0]
             # si le paramètre est autre chose qu'un chiffre pu un retour à la ligne (\n) il redevient à son état de base et arrète de lire
-            for characters in parametres[4]:
-                if ((characters < '0' or characters > '9') and characters != '\n'):
-                    self.iface.messageBar().clearWidgets()
-                    self.iface.messageBar().pushMessage("Attention", "paramètre d'angle invalide".format(str(filename)), level=Qgis.Critical, duration=10)
-                    angle = 10
-                    break
-            if (angle == 0):
-                angle = int(parametres[4])
+            angle = [int(d) for d in re.findall(r'-?\d+',parametres[4])]
+            if angle == []:
+                angle = 10
+                self.iface.messageBar().clearWidgets()
+                self.iface.messageBar().pushMessage("Attention", "paramètre d'angle invalide, utilisation de la valeur par défaut".format(str(filename)), level=Qgis.Critical, duration=10)
+            else:
+                angle = angle[0]
             # gestion d'erreur pour un angle invalide (dans ce cas si angle est plus grand de 50°)
             if (angle > 50):
                 angle = 10
                 self.iface.messageBar().clearWidgets()
-                self.iface.messageBar().pushMessage("Attention", "paramètre d'angle invalide".format(str(filename)), level=Qgis.Critical, duration=10)
+                self.iface.messageBar().pushMessage("Attention", "paramètre d'angle invalide, utilisation de la valeur par défaut".format(str(filename)), level=Qgis.Critical, duration=10)
 
             # lis la deuxième ligne (si elle existe) de la même manière que la première
             if line_number >= 6:
-                for characters in parametres[5]:
-                    if ((characters < '0' or characters > '9') and characters != '\n' and characters !='.'):
-                        self.iface.messageBar().clearWidgets()
-                        self.iface.messageBar().pushMessage("Attention", "paramètre de distance minimale invalide".format(str(filename)), level=Qgis.Critical, duration=10)
-                        distance = 0.01
-                        break
-                if (distance == 0):
-                    distance = float(parametres[5])
+                distance = [int(d) for d in re.findall(r'-?\d+.?\d+',parametres[4])]
+                if distance == []:
+                    self.iface.messageBar().clearWidgets()
+                    self.iface.messageBar().pushMessage("Attention", "paramètre de distance minimale invalide, utilisation de la valeur par défaut".format(str(filename)), level=Qgis.Critical, duration=10)
+                    distance = 0.01
+                else:
+                    distance = distance[0]
             else:
                 distance = 0.01
         else:
@@ -57,7 +55,7 @@ def read(self):
         return parametres
     else:
         self.iface.messageBar().clearWidgets()
-        self.iface.messageBar().pushMessage("Attention", "{} n'a pas pu être ouvert".format(str(filename)), level=Qgis.Critical, duration=10)
+        self.iface.messageBar().pushMessage("Attention", "{} n'a pas pu être ouvert, utilisation des valeurs par défaut".format(str(filename)), level=Qgis.Critical, duration=10)
         return [10, 0.01]
 
 # récupère le nombre total d'objets sur le quel le contrôle va travailler (actuellement seul les lignes)
@@ -88,10 +86,6 @@ def nb_for_tuple(self, str):
 # execution du controle
 def rebroussement(self, func):
     nom_controle = "rebroussement"
-    # objets_controle = ["limite_administrative", "ligne_frontalière", "tronçon_hydrographique", "limite_terre_mer"
-    #            , "histolitt", "ligne_électrique", "canalisation", "construction_linéaire", "ligne_orographique"
-    #            , "troncon_de_route", "densification_des_chemins", "tronçon_de_voie_ferrée", "transport_par_câble", "voie_de_triage"
-    #            , "itinéraire_ski_de_randonnée", "haie", "ligne_caractéristique", "limites_diverses", "modification_d_attribut"]
     for item in self.dlg_controles.treeWidget.findItems("rebroussement", QtCore.Qt.MatchRecursive):
         # vérifie si le contrôle "rebroussement" est coché et si il existe des objets de type Ligne
         if item.checkState(0) == 2:
@@ -121,20 +115,12 @@ def rebroussement(self, func):
                             for f in layers.getFeatures():
                                 # récupère la géométrie dans ces infos
                                 geom = f.geometry()
-                                # print('Area :', geom.area())
-                                # print('Perimeter :', geom.length())
-                                # print('Type :', QgsWkbTypes.displayString(geom.wkbType()))
                                 # récupère les informations nécéssaires dans la géométrie tel que le nom, le type, et les points
                                 for part in geom.parts():
                                     # mets a jour le progrès de la bar de progrès
                                     if ("LineString" not in QgsWkbTypes.displayString(part.wkbType())):
                                         break
                                     bar.setValue(items_done)
-                                    # mets comme type de données l'ESPG 2154
-                                    # part.transform(QgsCoordinateTransform(
-                                    #     QgsCoordinateReferenceSystem("IGNF:LAMB93"),
-                                    #     QgsCoordinateReferenceSystem("EPSG:2154"),
-                                    #     QgsProject.instance()))
                                     # parse le WKT de la géométrie pour avoir accès a chaque chiffre en tant que floats
                                     nums = re.findall(r'\-?[0-9]+(?:\.[0-9]*)?', part.asWkt()) # regex cherche entre chaque virgule: au moins un chiffre, puis un point, puis une chiffre si il y en a un, avec des parenthèses optionellement
                                     coords = tuple(zip(*[map(float, nums)] * nb_for_tuple(self, part.asWkt()))) # récupère les coordonnées en float et les ajoutes dans un tableau de floats pour une utilisation facile des données antérieurement
@@ -169,76 +155,3 @@ def rebroussement(self, func):
                 self.iface.messageBar().pushMessage("Info", "Contrôle {} impossible: il n'y a pas d'objets de type \"Ligne\". Passage au suivant".format(str(nom_controle)), level=Qgis.Warning, duration=10)
                 self.controles_restants += 1
                 return 2
-
-# different types of geometries:
-
-# Unknown: Unknown
-# Point: Point
-# LineString: LineString
-# Polygon: Polygon
-# Triangle: Triangle
-# MultiPoint: MultiPoint
-# MultiLineString: MultiLineString
-# MultiPolygon: MultiPolygon
-# GeometryCollection: GeometryCollection
-# CircularString: CircularString
-# CompoundCurve: CompoundCurve
-# CurvePolygon: CurvePolygon
-# MultiCurve: MultiCurve
-# MultiSurface: MultiSurface
-# PolyhedralSurface: PolyhedralSurface
-# Added in version 3.40.
-# TIN: TIN
-# Added in version 3.40.
-# NoGeometry: No geometry
-# PointZ: PointZ
-# LineStringZ: LineStringZ
-# PolygonZ: PolygonZ
-# TriangleZ: TriangleZ
-# MultiPointZ: MultiPointZ
-# MultiLineStringZ: MultiLineStringZ
-# MultiPolygonZ: MultiPolygonZ
-# GeometryCollectionZ: GeometryCollectionZ
-# CircularStringZ: CircularStringZ
-# CompoundCurveZ: CompoundCurveZ
-# CurvePolygonZ: CurvePolygonZ
-# MultiCurveZ: MultiCurveZ
-# MultiSurfaceZ: MultiSurfaceZ
-# PolyhedralSurfaceZ: PolyhedralSurfaceZ
-# TINZ: TINZ
-# PointM: PointM
-# LineStringM: LineStringM
-# PolygonM: PolygonM
-# TriangleM: TriangleM
-# MultiPointM: MultiPointM
-# MultiLineStringM: MultiLineStringM
-# MultiPolygonM: MultiPolygonM
-# GeometryCollectionM: GeometryCollectionM
-# CircularStringM: CircularStringM
-# CompoundCurveM: CompoundCurveM
-# CurvePolygonM: CurvePolygonM
-# MultiCurveM: MultiCurveM
-# MultiSurfaceM: MultiSurfaceM
-# PolyhedralSurfaceM: PolyhedralSurfaceM
-# TINM: TINM
-# PointZM: PointZM
-# LineStringZM: LineStringZM
-# PolygonZM: PolygonZM
-# MultiPointZM: MultiPointZM
-# MultiLineStringZM: MultiLineStringZM
-# MultiPolygonZM: MultiPolygonZM
-# GeometryCollectionZM: GeometryCollectionZM
-# CircularStringZM: CircularStringZM
-# CompoundCurveZM: CompoundCurveZM
-# CurvePolygonZM: CurvePolygonZM
-# MultiCurveZM: MultiCurveZM
-# MultiSurfaceZM: MultiSurfaceZM
-# PolyhedralSurfaceZM: PolyhedralSurfaceM
-# TINZM: TINZM
-# TriangleZM: TriangleZM
-# Point25D: Point25D
-# LineString25D: LineString25D
-# Polygon25D: Polygon25D
-# MultiPoint25D: MultiPoint25D
-# MultiLineString25D: MultiLineString25D
-# MultiPolygon25D: MultiPolygon25D
